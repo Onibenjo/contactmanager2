c19fe79a6f1bdeb8e440ea8e97a293994763b328
Onibenjo <onibenjo@gmail.com>
2019-04-07 01:25:33 +0100

    Updates

---
+++ precache-manifest.8e2112b17c7fd72c8289fff54219759b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.4bdee9f6.js"
+  },
+  {
+    "revision": "b2a4e360068ef51abbc9",
+    "url": "/contactmanager/static/js/main.d264ef03.chunk.js"
+  },
+  {
+    "revision": "32cdb34825d29ff04e5f",
+    "url": "/contactmanager/static/js/2.966aa64c.chunk.js"
+  },
+  {
+    "revision": "b2a4e360068ef51abbc9",
+    "url": "/contactmanager/static/css/main.83d57c1f.chunk.css"
+  },
+  {
+    "revision": "056ed1d5c9b2b152e4fff1c61a56a20e",
+    "url": "/contactmanager/index.html"
+  }
+];
